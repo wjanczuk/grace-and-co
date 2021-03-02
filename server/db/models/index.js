94fa234f417329b803da0359fbe16038b180d9e8
Weronika Janczuk <weronika.janczuk@gmail.com>
2021-03-02 13:25:28 -0600

    Completed db models, db associations, seed file; receiving seed error

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,16 @@
 const User = require('./user')
+const Product = require('./product')
+const Cart = require('./cart')
+const CartItem = require('./cartItem')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Cart.belongsToMany(Product, {through: CartItem})
+Product.belongsToMany(Cart, {through: CartItem})
+User.hasMany(Cart)
+Cart.belongsTo(User)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
-  User
+  User,
+  Product,
+  Cart,
+  CartItem
 }
