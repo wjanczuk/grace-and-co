3b29bb10bd5479388fdb6bf69d28cc7903277047
Michelle Dacal <michdacal@gmail.com>
2021-03-03 16:05:20 -0500

    BE: added routes for cartItems

--- server/api/cartItems.js
+++ server/api/cartItems.js
@@ -1,7 +1,50 @@
-//PUT request for a new cart item
-//include the below instance method
-Cart.prototype.updateCartTotals = async function(price, quantity) {
-  this.orderSubtotal += price * quantity
-  this.totalQuantity += quantity
-  await this.save()
-}
+const router = require('express').Router()
+const {CartItem} = require('../db/models')
+
+// GET /api/cartItems/:cartId
+router.get('/:cartId', async (req, res, next) => {
+  try {
+    const cartItems = await CartItem.findAll({
+      where: {
+        cartId: req.params.cartId
+      }
+    })
+    res.json(cartItems)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// DELETE /api/cartItems
+router.delete('/', async (req, res, next) => {
+  try {
+    await CartItem.destroy({
+      where: {
+        id: req.body.itemId
+      }
+    })
+    res.sendStatus(200)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT /api/cartItems
+router.put('/', async (req, res, next) => {
+  try {
+    await CartItem.update(
+      {
+        quantity: req.body.quantity
+      },
+      {
+        where: {
+          id: req.body.itemId
+        }
+      }
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
